c79cecb218f985df79918dbc0fbb7bd749860874
Ryan <ryeean16@gmail.com>
2025-10-03 02:54:16 -0700

    update

--- SpeedieApp/src/types/WarningLight.ts
+++
@@ -1,9 +0,0 @@
-export type WarningLight = {
-  id: string;
-  name: string;
-  description: string;
-  severity: "info" | "warning" | "critical";
-  image: string;
-  urgency: string;
-  fixInfo: string;
-}; 
